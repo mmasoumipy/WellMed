bcd60643d80f382e7af6c2074a837967376fca30
Mina Masoumi <mina.masoumi@concordia.ca>
2025-05-05 13:06:36 -0400

    add notifications

--- mobile/WellMed/src/utils/notifications.ts
+++ mobile/WellMed/src/utils/notifications.ts
@@ -6,6 +6,8 @@ export const scheduleMonthlyMBIReminder = () => {
     title: 'Monthly MBI Check-in',
     message: 'It’s time for your monthly Maslach Burnout Inventory assessment!',
     date: new Date(Date.now() + 60 * 1000), // TEST: triggers in 1 minute
-    repeatType: 'minute', // Repeat every month
+    repeatType: 'week', // Repeat every week
+    repeatTime: 7, // Repeat every 7 days
+    allowWhileIdle: true,
   });
 };
