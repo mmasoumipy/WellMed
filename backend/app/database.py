8e587e1ffe7a22a58717ddad2e2480e235a64b76
Mina Masoumi <mina.masoumi@concordia.ca>
2025-05-13 15:18:05 -0400

    update database file and Base

--- backend/app/database.py
+++ backend/app/database.py
@@ -14,6 +14,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from app import models
 import os
 
 DATABASE_URL = os.getenv('DATABASE_URL')
